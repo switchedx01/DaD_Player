fe2d6ea881642343334406ac32c9ce8bc2b0c30f
switchedx01 <cdharper3k@gmail.com>
2025-06-05 19:34:04 -0500

    Update icon_button.py
    Removed unnecessary comments for when debugging to get bare running program

--- dad_player/ui/widgets/icon_button.py
+++ dad_player/ui/widgets/icon_button.py
@@ -4,9 +4,8 @@ from kivy.properties import StringProperty, ListProperty, NumericProperty
 from kivy.graphics import Color, Ellipse, Rectangle
 from kivy.clock import Clock
 from kivy.logger import Logger
-from kivy.metrics import sp # Import Kivy's sp metric
+from kivy.metrics import sp
 
-# from dad_player.utils import spx # No longer using spx from utils here
 
 class IconButton(Button):
     icon_text = StringProperty('')
@@ -15,8 +14,7 @@ class IconButton(Button):
     icon_color_normal = ListProperty([0.9, 0.9, 0.9, 1])
     icon_color_down = ListProperty([0.95, 0.95, 0.95, 1])
     icon_font_name = StringProperty("Roboto")
-    # Renamed to reflect it should be an sp value now, or handled as a direct pixel value if sp() is applied
-    icon_font_size_sp = NumericProperty(sp(16)) # Default to sp(16)
+    icon_font_size_sp = NumericProperty(sp(16))
 
     _current_circle_color = ListProperty([0,0,0,0])
     _current_icon_color = ListProperty([0,0,0,0])
@@ -25,15 +23,15 @@ class IconButton(Button):
         super().__init__(**kwargs)
         self.background_normal = ''
         self.background_down = ''
-        self.background_color = [0, 0, 0, 0] # Transparent background for custom drawing
+        self.background_color = [0, 0, 0, 0]
 
         self._current_circle_color = self.circle_color_normal
         self._current_icon_color = self.icon_color_normal
         
         self.bind(
-            icon_text=self._update_text_and_font, # Combined update
-            icon_font_size_sp=self._update_text_and_font, # Combined update
-            icon_font_name=self.setter('font_name'), # Directly set Button's font_name
+            icon_text=self._update_text_and_font,
+            icon_font_size_sp=self._update_text_and_font,
+            icon_font_name=self.setter('font_name'),
             circle_color_normal=self._update_colors_from_normal,
             icon_color_normal=self._update_colors_from_normal,
             pos=self._redraw_canvas,
@@ -43,7 +41,7 @@ class IconButton(Button):
         
         self._update_text_and_font() # Initial setup for text, font_size
         # self.draw_custom_graphics() # Initial draw, or let pos/size binding handle it
-        Clock.schedule_once(self.draw_custom_graphics, 0.01) # Ensure layout has settled
+        Clock.schedule_once(self.draw_custom_graphics, 0.01)
 
     def _update_text_and_font(self, *args):
         self.text = self.icon_text         # Set Button's text to the icon character
@@ -87,11 +85,7 @@ class IconButton(Button):
             ellipse_y = self.center_y - diameter / 2
             Ellipse(pos=(ellipse_x, ellipse_y), size=(diameter, diameter))
         
-        # The Button's text (icon) will be drawn by the Button itself using self.color.
-        # self.color = self._current_icon_color # Already set in _on_state_change
 
 class IconOnlyButton(IconButton): # Example of a variant if needed
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # Example: Different default colors or font size for this specific type
-        # self.icon_font_size_sp = sp(20) 
